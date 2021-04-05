12857c387121931c1855eff1518b7c2f3d43760f
sylvain <sylvain@SEJEN-Sylvain.local>
2021-04-05 03:33:02 +0000

    Add redirect action from /  to api todos list

--- api/urls.py
+++ api/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
+from django.views.generic.base import RedirectView
 from .views import TodoDetailView, TodosListView
 
 app_name = 'api'
 
 urlpatterns = [
+    path('', RedirectView.as_view(url='/api/todos/')),
     path('todos/', TodosListView.as_view(), name='todo-list'),
     path('todo/<int:pk>/', TodoDetailView.as_view(), name='todo-detail'),
 ]
